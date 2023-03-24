9000d80bd5420bee9d46934b572fcd00ea5e3fae
mac <ryjun91@github.com>
2023-03-24 21:23:19 +0900

    FEAT FIRST COMMIT

---
+++ src/main/java/com/example/instagram_clonecoding/InstagramClonecodingApplication.java
@@ -0,0 +1,13 @@
+package com.example.instagram_clonecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramClonecodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstagramClonecodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/instagram_clonecoding/InstagramClonecodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.instagram_clonecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramClonecodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
