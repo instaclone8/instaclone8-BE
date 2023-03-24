bb13da40bef928c8587238354534f3d580b28207
DongHee980630 <ehdehdrnt5@gmail.com>
2023-03-24 22:23:15 +0900

    FEAT : userSignup

--- src/main/java/com/example/instaclone/user/entity/User.java
+++ src/main/java/com/example/instaclone/user/entity/User.java
@@ -22,7 +22,7 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
+    @Column(nullable = true)
     private String userImage;
 
     @Column(nullable = false)
