d94d65fcf036dc148765450b99992dcd33b19dda
bambee <daj0909@naver.com>
2023-03-24 17:13:59 +0900

    fix : User, Security, jwt, config 패키지      application.properties      USER 예외처리 후 API 기록 필요

--- src/main/java/com/example/instaclone/user/controller/UserController.java
+++ src/main/java/com/example/instaclone/user/controller/UserController.java
@@ -6,10 +6,7 @@ import com.example.instaclone.user.dto.SignupRequestDto;
 import com.example.instaclone.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
@@ -32,6 +29,10 @@ public class UserController {
             HttpServletResponse response) {
         return ResponseEntity.ok().body(userservice.login(loginRequestDto, response));
     }
+
+//    마이페이지
+//    @GetMapping("/mypage{userId}")
+
 
 
 
--- src/main/java/com/example/instaclone/user/dto/LoginRequestDto.java
+++ src/main/java/com/example/instaclone/user/dto/LoginRequestDto.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.NotBlank;
 @Getter
 public class LoginRequestDto {
 
-    @NotBlank(message = "아이디를 입력해 주세요")
+    @NotBlank(message = "이메일을 입력해 주세요")
     private String email;
     @NotBlank(message = "비밀번호를 입력해 주세요")
     private String password;
