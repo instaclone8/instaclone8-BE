0973ee515d32b8f0407b34cdb2af15c5d3cdea82
bambee <daj0909@naver.com>
2023-03-25 17:31:53 +0900

    feat : 머지중

--- src/main/java/com/example/instaclone/InstacloneApplication.java
+++ src/main/java/com/example/instaclone/InstacloneApplication.java
@@ -2,8 +2,9 @@ package com.example.instaclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@EnableJpaAuditing @SpringBootApplication
 public class InstacloneApplication {
 
     public static void main(String[] args) {
