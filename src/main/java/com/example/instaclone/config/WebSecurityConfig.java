d029ebdd4b8beec965a257e604eae8e0e1662e5b
bambee <daj0909@naver.com>
2023-03-27 17:41:23 +0900

    feat : config 변경

--- src/main/java/com/example/instaclone/config/WebSecurityConfig.java
+++ src/main/java/com/example/instaclone/config/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig {
                 .antMatchers( "/api/user/checkemail").permitAll()
                 .antMatchers( "/api/user/kakao/callback").permitAll()
                 .antMatchers("/api/user/checkusername").permitAll()
-//                  .antMatchers( "/s3/**").permitAll()
+                  .antMatchers( "/s3/**").permitAll()
                 .anyRequest().authenticated()
                 .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(new ApiException(), JwtAuthFilter.class);
